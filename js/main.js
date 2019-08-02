0520714b444e8217626f87afefa2900f542c3063
trs1323 <45774674+trs1323@users.noreply.github.com>
2019-08-02 11:12:42 -0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//animate smooth scoll
+$('#view-work').on('click', function() {
+  const images= $('#images').position().top;
+
+  $('html, body').animate(
+    {
+    scrollTop: images
+  },
+   900);
+});
